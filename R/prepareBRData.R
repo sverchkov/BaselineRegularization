62de945716e5320cedef3f1f2c59f74088b4c2bf
Yuriy Sverchkov <yuriy@biostat.wisc.edu>
2017-12-05 11:49:24 -0600

    progress on data processing

--- R/prepareBRData.R
+++ R/prepareBRData.R
@@ -1,14 +1,85 @@
 #' Create data object
 #'
 #' Creates the data object for running Baseline Regularization
-#' @param data A dplyr tibble that contains the columns [...]
-#' @param event Event of interest
+#' @param observation_period A dataframe-like view of the OMOP OBSERVATION_PERIOD table
+#' @param drug_era A dataframe-like view of the OMOP DRUG_ERA table
+#' @param condition_era A dataframe-like view of the OMOP CONDITION_ERA table
+#' @param condition The condition_concept_id of the condition of interest
 #' @param tying Parameter tying mode, "none", "interval", or "occurence" (default)
 #' @param risk_window The number of days right after a drug era during which the patient is considered still under exposure
 #' @param minimum_duration The number of days a patient must be under observation to be included in the analysis
+#' @param independent_observation_periods Whether to treat distinct observation periods from one patient as distinct "patients"
 #' @return An object containing the matrices X, Z, y
 #' @export
-prepareBRData <- function ( data, event, tying = "occurence", risk_window = 0, minimum_duration = 0 ){
+prepareBRData <- function ( observation_period
+                          , drug_era
+                          , condition_era
+                          , event
+                          , tying = "occurence"
+                          , risk_window = 0
+                          , minimum_duration = 0
+                          , independent_observation_periods = TRUE ){
+
+  if ( ! independent_observation_periods )
+    stop( "Current implementation only supports independent observation periods." )
+  else {
+    working_observation_periods <- filter( observation_period, observation_period_end_date - observation_period_end_date >= minimum_duration )
+  }
+
+  drug_era_events <- inner.join( working_observation_periods, drug_era, by = c( person_id = "person_id" ) )
+
+  if ( risk_window > 0 ){
+    # Risk window expansion
+    drug_era_events <- mutate( drug_era_events, drug_era_end_date = drug_era_end_date + risk_window )
+    # Check for each drug
+  }
+
+  drug_start_events <-
+    drug_era_events %>%
+    filter( drug_era_start_date >= observation_period_start_date, drug_era_start_date <= observation_period_end_date ) %>%
+    select( event_era_id = drug_era_id
+          , event_type = "drug start"
+          , person_id
+          , concept_id = drug_concept_id
+          , event_date = drug_era_start_date
+          , observation_period_id
+          , observation_period_start_date
+          , observation_period_end_date )
+
+  drug_end_events <-
+    drug_era_events %>%
+    select( event_era_id = drug_era_id
+          , event_type = "drug end"
+          , person_id
+          , concept_id = drug_concept_id
+          , event_date = drug_era_end_date + 1 # Check that this is how you add a day
+          , observation_period_id
+          , observation_period_start_date
+          , observation_period_end_date ) %>%
+    filter( event_date >= observation_period_start_date, event_date <= observation_period_end_date )
+
+  condition_events <-
+    condition_era %>%
+    select( event_era_id = condition_era_id
+          , event_type = "condition start"
+          , person_id
+          , concept_id = condition_concept_id
+          , event_date = condition_era_start_date
+          , observation_period_id
+          , observation_period_start_date
+          , observation_period_end_date ) %>%
+    filter( event_date >= observation_period_start_date, event_date <= observation_period_end_date )
+
+  # bind rows
+  events <- bind_rows( drug_start_events, drug_end_events, condition_events )
+
+  # the distinct time breaks become the intervals. We sort them
+  timebreaks <- events %>%
+    distinct( event_date, observation_period_id ) %>%
+    arrange( observation_period_if, event_date ) %>%
+    mutate( interval = row_number() )
+
+  # # # #
 
   stop("Not yet implemented")
 
@@ -47,6 +118,10 @@ prepareBRData <- function ( data, event, tying = "occurence", risk_window = 0, m
 
   # Return
   list(
-    X = X
+    X = X,
+    Z = Z,
+    l = l,
+    n = n,
+    patients = patients
   )
 }
