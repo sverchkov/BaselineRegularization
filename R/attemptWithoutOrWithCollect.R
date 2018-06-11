c2a2c1ba41b911b5c18184219b6af11472ab385e
Yuriy Sverchkov <yuriy@biostat.wisc.edu>
2018-06-11 13:39:31 -0500

    DB function availability check was failing, explicitly checking for SQLite instead

--- R/attemptWithoutOrWithCollect.R
+++ R/attemptWithoutOrWithCollect.R
@@ -1,7 +1,6 @@
 #' Attempt to execute a function on a data table, maybe collect
 #'
-#' Tries to apply a function to a table, if that fails with an error, tries again after calling collect() on the table.
-#' This is to get around functions that aren't supported in some DBs.
+#' If the table is from an sqlite connection, collects before executing function.
 #'
 #' @param the_table the table
 #' @param the_function the function
@@ -9,14 +8,12 @@
 #' @import dplyr
 #' @import futile.logger
 attemptWithoutOrWithCollect <- function ( the_table, the_function ) {
-  flog.debug("Entering attemptWithoutOrWithCollect")
-  result <- tryCatch(
-    the_function( the_table ),
-    error = function( e ){
-      flog.warn( "Caught %s, trying workaround.", as.character(e) )
-      the_function( collect( the_table ) )
-    }
-  )
-  flog.debug("Exiting attemptWithoutOrWithCollect")
-  return ( result )
+
+  # Check for sqlite connection
+  if( "SQLiteConnection" %in% class( the_table$src$con ) ) {
+    flog.warn( "Detected SQLite Connection, avoiding unsupported op" )
+    the_function( collect( the_table ) )
+  } else {
+    the_function( the_table )
+  }
 }
