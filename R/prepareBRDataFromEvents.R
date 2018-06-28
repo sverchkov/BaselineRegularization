f069e763ee83c37a1fde2bd8644d365f3cccd736
Yuriy Sverchkov <yuriy@biostat.wisc.edu>
2018-06-28 16:17:01 -0500

    Updated prepareBR to use day-based dates

--- R/prepareBRDataFromEvents.R
+++ R/prepareBRDataFromEvents.R
@@ -8,21 +8,19 @@ prepareBRDataFromEvents <- function ( all_events, event, tying ){
 
   # Make events for the ends of observation periods
   obs_start_events <- all_events %>%
-    select( obs_period, event_date = observation_period_start_date, observation_period_end_date ) %>%
-    distinct()
+    distinct( obs_period, observation_period_length ) %>%
+    mutate( event_day = 0L )
 
   # Make list of event times
   event_times <- all_events %>%
     # We just need the times
-    select( obs_period, event_date, observation_period_end_date ) %>%
+    distinct( obs_period, event_day, observation_period_length ) %>%
     # Add the start-of-observation events
-    union_all( obs_start_events ) %>%
-    # Get distinct
-    distinct() %>%
+    union( obs_start_events ) %>%
     # Get days to end of obs period for each event
-    transmute( obs_period, event_date, days_to_end = observation_period_end_date - event_date ) %>%
+    transmute( obs_period, event_day, days_to_end = observation_period_length - event_day ) %>%
     # Sort
-    arrange( obs_period, event_date ) %>%
+    arrange( obs_period, event_day ) %>%
     # Get interval numbering
     mutate( interval_number = row_number( ) ) %>%
     # Group by observation periods
@@ -34,45 +32,29 @@ prepareBRDataFromEvents <- function ( all_events, event, tying ){
     # Ungroup
     ungroup() %>%
     # Clean up
-    select( obs_period, event_date, interval_number, interval_length )
+    select( obs_period, event_day, interval_number, interval_length )
 
   # Get sperse representations to prepare feature matrices
   feature_indeces <- all_events %>%
     # Features don't contain the target event
     filter( concept_id != event ) %>%
     # Get event features
-    left_join( event_times, by = c( obs_period = "obs_period", event_date = "event_date" ) ) %>%
+    left_join( event_times, by = c( obs_period = "obs_period", event_day = "event_day" ) ) %>%
     # Get a dense numbering of drugs
     mutate( drug_number = dense_rank( concept_id ) )
 
   # ADE occurences
-  ade_intervals <- if ( T ){
-    all_events %>%
-      filter( concept_id == event ) %>%
-      # We only need obs period and event date, and we don't count multiple occurrences per interval
-      distinct( obs_period, event_date ) %>%
-      # Get interval numbers
-      inner_join( event_times, by = c( obs_period = "obs_period", event_date = "event_date" ) ) %>%
-      # We only need interval numbers
-      select( interval_number ) %>%
-      # Sort by intervals
-      arrange( interval_number )
-  } else { # Keeping this code here in case we want to count ADEs per interval
-    all_events %>%
-      filter( concept_id == event ) %>%
-      # We only need obs period and event date
-      select( obs_period, event_date ) %>%
-      # Get interval numbers
-      inner_join( event_times, by = c( obs_period = "obs_period", event_date = "event_date" ) ) %>%
-      # We only need interval numbers
-      select( interval_number ) %>%
-      # Sort by intervals
-      arrange( interval_number )
-  }
+  ade_intervals <- all_events %>%
+    filter( concept_id == event ) %>%
+    # We only need obs period and event date, and we don't count multiple occurrences per interval
+    distinct( obs_period, event_day ) %>%
+    # Get interval numbers
+    inner_join( event_times, by = c( obs_period = "obs_period", event_day = "event_day" ) ) %>%
+    # We only need interval numbers
+    select( interval_number ) %>%
+    # Sort by intervals
+    arrange( interval_number )
 
-  ##
-  ## Everything above this point didn't require executing the SQL if we were working in a db
-  ##
 
   # We'll be referring to the number of intervals a lot
   number_of_intervals <- as.integer( (
@@ -105,7 +87,7 @@ prepareBRDataFromEvents <- function ( all_events, event, tying ){
 
                 start_intervals <- obs_start_events %>%
                   # Get interval numbers
-                  inner_join( event_times, by = c( obs_period = "obs_period", event_date = "event_date" ) ) %>%
+                  inner_join( event_times, by = c( obs_period = "obs_period", event_day = "event_day" ) ) %>%
                   # We only need interval numbers
                   select( interval_number )
 
