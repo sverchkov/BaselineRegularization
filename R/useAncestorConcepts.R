cf139b7cb41bfd232b7e0eb70093c0b9c2f9c5e2
Yuriy Sverchkov <yuriy@biostat.wisc.edu>
2018-07-03 14:07:24 -0500

    Added method to use ancestor concepts

---
+++ R/useAncestorConcepts.R
@@ -0,0 +1,22 @@
+#' Use ancestor concepts
+#'
+#' Use an ancestry table to add a column of ancestor concepts to a record table
+#'
+#' @import dplyr
+#' @export
+useAncestorConcepts <- function ( record_table,
+                                  ancestors,
+                                  record_table_column = "concept_id",
+                                  ancestor_column = "ancestor_concept_id",
+                                  descendant_column = "descendant_concept_id") {
+
+  by_map <- descendant_column
+  names( by_map ) <- record_table_column
+
+  left_join(
+    record_table,
+    ancestors %>% distinct(
+      `!!`(rlang::sym( descendant_column )),
+      `!!`(rlang::sym( ancestor_column )) ),
+    by = by_map )
+}
