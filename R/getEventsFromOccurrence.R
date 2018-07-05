a3d15a85bfb17d995be19b8f8395d7931f0d021a
Yuriy Sverchkov <yuriy@biostat.wisc.edu>
2018-07-05 14:19:46 -0500

    Added proper lasting risk window handling

--- R/getEventsFromOccurrence.R
+++ R/getEventsFromOccurrence.R
@@ -1,5 +1,19 @@
-#' Make events table from occurrence information
+#' Make events table from occurrence information (internal function)
 #'
+#' Make events table for [prepareBRDataFromEvents] from occurrence tables (rather than era tables).
+#'
+#' Drug exposure durations are calculated as: exposure end date - exposure start date (if end date is specified) or
+#' according to `days_supply` (if specified) or 1 day if neither is specified, plus `risk_window`.
+#'
+#' @param drug_exposure A dataframs-like object correspondint to the OMOP CDM `drug_exposure` table
+#' @param condition_occurrence A dataframs-like object correspondint to the OMOP CDM `condition_occurrence` table
+#' @param visit_occurrence A dataframs-like object correspondint to the OMOP CDM `visit_occurrence` table
+#' @param event The event of interest
+#' @param risk_window See note on drug exposure duration calculation
+#' @param minimum_duration The minimum duration that a patient needs to be under observation to be included in the
+#' analysis.
+#'
+#' @author Yuriy Sverchkov
 #' @import dplyr
 #' @import futile.logger
 getEventsFromOccurrence <- function( drug_exposure
@@ -11,8 +25,10 @@ getEventsFromOccurrence <- function( drug_exposure
                                    )
 {
   # Ensure correct types
-  minimum_duration = as.integer( minimum_duration )
-  risk_window = as.integer( risk_window )
+  minimum_duration <- as.integer( minimum_duration )
+
+  lasting_risk <- Inf == risk_window
+  if ( !lasting_risk ) risk_window <- as.integer( risk_window )
 
   # Derive observation periods
   observation_periods <-
@@ -44,12 +60,16 @@ getEventsFromOccurrence <- function( drug_exposure
                observation_period_length,
                concept_id = drug_concept_id,
                drug_start_day,
-               drug_end_day = 1L + drug_start_day + if_else( is.na( drug_exposure_end_date ), 0L, as.integer( drug_exposure_end_date - drug_exposure_start_date ) )
+               drug_end_day =
+                 1L + drug_start_day +
+                 if_else( is.na( drug_exposure_end_date ),
+                          if_else( is.na( days_supply ), 0L, days_supply ),
+                          as.integer( drug_exposure_end_date - drug_exposure_start_date ) )
     )
 
-  if ( risk_window > 0 )
+  if ( !lasting_risk && risk_window > 0 )
     drug_durations <- drug_durations %>%
-      mutate( drug_end_dy = drug_end_day + risk_window )
+      mutate( drug_end_day = drug_end_day + risk_window )
 
 
   # Make table in the db since it will be reused
@@ -57,20 +77,26 @@ getEventsFromOccurrence <- function( drug_exposure
   # if( "tbl_dbi" %in% class( drug_durations ) ) drug_durations %>% explain()
   drug_durations <- drug_durations %>% compute()
 
-  # Return value
-  union_all( drug_durations %>%
+  events_result <- drug_durations %>%
     transmute( person_id, concept_id,
                event_day = drug_start_day,
                event_flag = 1L,
                obs_period_id,
-               observation_period_length ),
-    drug_durations %>%
-      transmute( person_id, concept_id,
-                 event_day = drug_end_day,
-                 event_flag = -1L,
-                 obs_period_id,
-                 observation_period_length ) ) %>%
-    union_all( condition_occurrence %>%
+               observation_period_length )
+
+  if( !lasting_risk )
+    events_result <- events_result %>% union_all(
+      drug_durations %>%
+        transmute( person_id, concept_id,
+                   event_day = drug_end_day,
+                   event_flag = -1L,
+                   obs_period_id,
+                   observation_period_length )
+    )
+
+  # Return value
+  union_all( events_result,
+             condition_occurrence %>%
                  inner_join( observation_periods, by = "person_id" ) %>%
                  mutate( event_day = as.integer( condition_start_date - observation_period_start ) ) %>%
                  filter( condition_concept_id == event,
