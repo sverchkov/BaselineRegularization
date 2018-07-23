9206e28e033a6787d4d270ff884c3960181f133e
Yuriy Sverchkov <yuriy@biostat.wisc.edu>
2018-07-23 11:53:20 -0500

    Generalized getConditionEvents

--- R/getConditionEvents.R
+++ R/getConditionEvents.R
@@ -1,10 +1,27 @@
+#' Get condition events from a condition occurrence/era table and an observation period table
+#'
+#' Get condition events from a condition occurrence/era table and an observation period table:
+#' takes the start date to be the event time, merges on person_id with observation periods, and filters to keep the event
+#' within the observation period.
+#'
+#' Defaults work for the condition_occurrence table, need to specify date column for condition era
+#' (`condition_era_start_date` according to OMOP CDM)
+#'
+#' @param condition the condition occurrence or condition era table
+#' @param observation_period the observation period table
+#' @param date_column (NSE evaluation) the column in the condition table that should be used as the condition event time,
+#' `condition_start_date` by default.
+#' @return a table of condition events
 #'
 #' @import dplyr
 #' @importFrom rlang .data
 #' @author Yuriy Sverchkov
-getConditionEvents <- function ( condition_occurrence, observation_periods ){
-  inner_join( condition_occurrence, observation_periods, by = "person_id" ) %>%
-    mutate( event_day = as.integer( condition_start_date - observation_period_start_date ) ) %>%
+getConditionEvents <- function ( condition, observation_period, date_column = !!as.symbol("condition_start_date") ){
+
+  date_sym <- rlang::enexpr( date_column )
+
+  inner_join( condition, observation_period, by = "person_id" ) %>%
+    mutate( event_day = as.integer( (!!date_sym) - observation_period_start_date ) ) %>%
     filter( event_day >= 0,
             event_day <= observation_period_length ) %>%
     transmute( concept_id = condition_concept_id,
