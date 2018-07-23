39ddee0401e92d7e5bd11399a4ab32d3902bc608
Yuriy Sverchkov <yuriy@biostat.wisc.edu>
2018-07-23 11:54:15 -0500

    Updated fromEras to use new functions

--- R/prepareBRDataFromEras.R
+++ R/prepareBRDataFromEras.R
@@ -32,11 +32,9 @@ prepareBRDataFromEras <- function ( con = NULL
   if ( !( tying %in% c("occurrence", "interval" ) ) )
     stop( flog.fatal( "Invalid 'tying' parameter supplied (%s).", tying ) )
 
-  if ( !is.null( con ) ){
-    observation_period <- getTable( con, observation_period, "observation period" )
-    drug_era <- getTable( con, drug_era, "drug era" )
-    condition_era <- getTable( con, condition_era, "condition era" )
-  }
+  observation_period <- getTable( con, observation_period, "observation period" )
+  drug_era <- getTable( con, drug_era, "drug era" )
+  condition_era <- getTable( con, condition_era, "condition era" )
 
   if ( ! independent_observation_periods )
     stop( flog.fatal( "Current implementation only supports independent observation periods." ) )
@@ -46,55 +44,24 @@ prepareBRDataFromEras <- function ( con = NULL
       filter( observation_period_length >= minimum_duration )
   }
 
-  #drug_era_events <- inner_join( working_observation_periods, drug_era, by = c( person_id = "person_id" ) )
+  # Get drug durations from drug eras
+  drug_durations <- inner_join( drug_era, working_observation_periods, by = "person_id" ) %>%
+    filter( drug_era_start_data >= observation_period_start_date, drug_era_end_date < observation_period_end_date ) %>%
+    transmute( observation_period_id,
+               observation_period_length,
+               concept_id = drug_concept_id,
+               drug_start_day = as.integer( drug_start_date - observatio_period_start_date ),
+               drug_end_day = as.integer( drug_end_date - observation_period_end_date ) ) %>%
+    mutate( drug_end_date = ifelse( !is.na( drug_end_date ), drug_end_day, drug_start_day ) + 1L )
 
-  # Risk window expansion
-  working_drug_era =
-    if ( risk_window > 0 ){
-      mutate( drug_era_events, drug_era_end_date = drug_era_end_date + risk_window )
-      # Check if we ended up merging drug eras
-      warning("Risk window expansion may have introduced drug era overlaps")
-    } else drug_era
+  # Get drug events
+  drug_events <- getDrugEvents( drug_durations, risk_window )
 
-  drug_start_events <- working_drug_era %>%
-    select( event_era_id = drug_era_id
-          , person_id
-          , concept_id = drug_concept_id
-          , event_date = drug_era_start_date ) %>%
-    mutate( event_flag = 1L )
+  # Derive condition days
+  condition_events <- getConditionEvents( condition_era, working_observation_periods, date_column = condition_era_start_date )
 
-  drug_end_events <- working_drug_era %>%
-    mutate( event_date = drug_era_end_date + 1L ) %>%
-    select( event_era_id = drug_era_id
-          , person_id
-          , concept_id = drug_concept_id
-          , event_date ) %>%
-    mutate( event_flag = -1L )
-  # Code notes on the above:
-  # We cast 1 to integer when adding to the date because dbplyr converts numerals to decimals by default
-  # We add 1 to the end date since the "event" is "no longer being exposed," this also folds in with how we turn this
-  # into a feature table later (the corresponding interval becomes the first to have a 0 for that drug after a run of
-  # 1s).
-  # For some reason doing the addition in the select statement gives an "object not found" error, so that's why it's
-  # in a mutate statement.
-
-  condition_events <- condition_era %>%
-    filter( condition_concept_id == response_event ) %>%
-    select( event_era_id = condition_era_id
-          , person_id
-          , concept_id = condition_concept_id
-          , event_date = condition_era_start_date ) %>%
-    mutate( event_flag = 1L )
-
-  # Get events associated with observation periods
-  all_events <- # combine events
-    drug_start_events %>% union_all( drug_end_events ) %>% union_all( condition_events ) %>%
-    # join with observation periods
-    inner_join( working_observation_periods, by = c( person_id = "person_id" ) ) %>%
-    # Compute days since observation period start
-    mutate( event_day = event_date - observation_period_start_date ) %>%
-    # filter to events falling within observation periods
-    filter( event_day >= 0, event_day <= observation_period_length )
+  # Derive events
+  events_table <- union_all( drug_events, filter( condition_events, concept_id == response_event ) )
 
   # Return
   prepareBRDataFromEvents( all_events, response_event, tying )
