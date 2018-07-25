f2255b561d7304a010e0432316357ace6547e9bb
Yuriy Sverchkov <yuriy@biostat.wisc.edu>
2018-07-25 12:26:47 -0500

    Moved getAbsErr to its own file and added docs

--- R/helper.R
+++ R/helper.R
@@ -83,30 +83,6 @@ getInterval = function(x,era){
     return(curr);
 }
 
-#' Get absolute error between a pair of vectors/matrices
-#'
-#' Get absolute error between a pair of vectors/matrices
-#'
-#' @import Matrix
-#' @import futile.logger
-getAbsErr = function( a, b ){
-    oldVec = Matrix( a );
-    newVec = Matrix( b );
-
-    d1 <- dim( oldVec )
-    d2 <- dim( newVec )
-    if( any( d1 != d2 ) ){
-      flog.error( "Trying to compare vectors of different size! (%s and %s)",
-                  deparse( enexpr( a ) ),
-                  deparse( enexpr( b ) ) )
-      flog.error( "Dimensions of first argument:", d1, capture = T )
-      flog.error( "Dimensions of second argument:", d2, capture = T )
-    }
-
-    relErr = norm(oldVec-newVec,type="F");
-    return(relErr);
-}
-
 parseConfig = function(x,configBr){
 
     # read
