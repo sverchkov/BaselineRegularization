625c3ac2e7319968b4606aaf8007573e100b840b
Yuriy Sverchkov <yuriy@biostat.wisc.edu>
2018-08-24 17:50:44 -0500

    Added data for vignettes

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' A mini subset of the synpuf dataset in OMOP CDM
+#'
+#' A mini subset of the synpuf dataset in OMOP CDM
+#'
+#' @docType data
+#' @usage data(synpud_mini)
+#' @format a list of dataframes: `observation_period`, `condition_occurrence`, `drug_exposure`
+#' @keywords datasets
+#' @references TODO: fill out
+#' @source synpuf (TODO: fill out)
+"synpuf_mini"
