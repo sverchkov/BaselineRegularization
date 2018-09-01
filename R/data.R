ed4703e02900b643182fa96cc8c7f5b1dae57035
Yuriy Sverchkov <yuriy@biostat.wisc.edu>
2018-09-01 13:10:27 -0500

    Added data doc

--- R/data.R
+++ R/data.R
@@ -11,3 +11,47 @@
 #' The CMS 2008-2010 Data Entrepreneurs’ Synthetic Public Use File (DE-SynPUF)
 #' https://www.cms.gov/Research-Statistics-Data-and-Systems/Downloadable-Public-Use-Files/SynPUFs/DE_Syn_PUF.html
 "synpuf_mini"
+
+#' A subset of the "concept" table for OMOP concepts
+#'
+#' A subset of the "concept" table for OMOP concepts
+#'
+#' @docType data
+#' @usage data("omop_doi_concept_ancestor")
+#' @format a data frame (tibble)
+#' @keywords datasets
+#' @source OMOP CDM Vocabulary tables
+"omop_concept_names"
+
+#' A subset of the "concept ancestor" table for OMOP concepts
+#'
+#' A subset of the "concept ancestor" table for OMOP concepts
+#'
+#' @docType data
+#' @usage data("omop_doi_concept_ancestor")
+#' @format a data frame (tibble) with columns `descendant_concept_id` and `ancestor_concept_id`
+#' @keywords datasets
+#' @source OMOP CDM Vocabulary tables
+"omop_doi_concept_ancestor"
+
+#' A subset of the OMOP CDM "concept ancestor" table
+#'
+#' A subset of the OMOP CDM "concept ancestor" table
+#'
+#' @docType data
+#' @usage data("omop_doi_concept_ancestor")
+#' @format a data frame (tibble)
+#' @keywords datasets
+#' @source OMOP CDM Vocabulary tables
+"cdm_some_concept_names"
+
+#' A mapping from OMOP HOIs to condition concept IDs
+#'
+#' A mapping from OMOP HOIs to condition concept IDs
+#'
+#' @docType data
+#' @usage data("omop_doi_concept_ancestor")
+#' @format a data frame (tibble)
+#' @keywords datasets
+#' @source OMOP CDM Vocabulary tables and HOI definitions
+"omop_hoi_conditions_map"
