e2193164a967780595e4c5ecd79e742d3061e2b9
Yuriy Sverchkov <yuriy@biostat.wisc.edu>
2018-09-13 16:20:56 -0500

    Added tests

---
+++ tests/testthat/test-fitBR.R
@@ -0,0 +1,20 @@
+context("fitBR")
+
+test_that("Regularized mini interval-tying single-patient model fit", {
+
+  # Tiny toy dataset:
+  # * 1 patients, with 14 intervals
+  # * 1 drug,
+  # Interval length: 5 2 1 5 10 10 5 10 4 7 3 12 20 12
+  # ADE:             0 0 1 0  1  0 0  0 0 1 0  0  0  0
+  # Drug:            0 1 1 1  1  0 0  0 1 1 1  1  0  0
+
+  X = Matrix::Matrix( c( 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0 ), nrow=14, ncol=1 )
+  Z = diag( 14 )
+  l = c( 5, 2, 1, 5, 10, 10, 5, 10, 4, 7, 3, 12, 20, 12 )
+  n = c( 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0 )
+  interval_obs_period = rep( 1, 14 )
+
+  fit = fitBR( Z, interval_obs_period, X, l, n, lambda1 = 0.01, lambda2=0.05, lambda3 = 0 )
+
+})
--- tests/testthat/test-fitBaselineRegularization.R
+++ tests/testthat/test-fitBaselineRegularization.R
@@ -37,3 +37,37 @@ test_that("mini interval-tying model fit", {
 
   expect_equal( fit$res, 1.226038e-09 )
 })
+
+test_that("regularized mini interval-tying model fit", {
+
+  # Tiny toy dataset:
+  # * 4 patients, with 5, 3, 4, 2 intervals respectively
+  # * 3 drugs, a, b, c. x marks adverse event, - marks empty interval, (#) marks length
+  # Patient 1: c(5) -(2) x(1) a(5) ax(10)
+  # Patient 2: a(10) ab(5) b(10)
+  # Patient 3: a(4) abx(7) b(3) bc(12)
+  # Patient 4: abc(20) -(12)
+
+  brData <- list(
+    X = Matrix::t( Matrix::Matrix(
+      c( 0,0,1, 0,0,0, 0,0,0, 1,0,0, 1,0,0,
+         1,0,0, 1,1,0, 0,1,0,
+         1,0,0, 1,1,0, 0,1,0, 0,1,1,
+         1,1,1, 0,0,0 ),
+      nrow=3, ncol=14 ) ),
+    Z = diag( 14 ),
+    l = c( 5, 2, 1, 5, 10,
+           10, 5, 10,
+           4, 7, 3, 12,
+           20, 12 ),
+    n = c( 0, 0, 1, 0, 1,
+           0, 0, 0,
+           0, 1, 0, 0,
+           0, 0 ),
+    patients = c( rep( 1, 5 ), rep( 2, 3 ), rep( 3, 4 ), rep( 5, 2 ) )
+  )
+
+  fit = fitBaselineRegularization( brData, defineBRParameters( lambda1 = 0.01 ) )
+
+  expect_equal( fit$res, 1.226038e-09 )
+})
